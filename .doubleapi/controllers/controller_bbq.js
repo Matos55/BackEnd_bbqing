5b6d8f9381249b78ceb425c9abc3e47e184880cf
João Matos <joaoapcm@gmail.com>
2021-01-28 06:16:21 +0000

    added SYNC/WAIT to API to improve FILTERS. Added 409 code

--- .doubleapi/controllers/controller_bbq.js
+++ .doubleapi/controllers/controller_bbq.js
@@ -13,19 +13,22 @@ const Model_bbq = require("../models/model_bbq.js");
 // delete
 */
 
-
-
 // CREATE (store)
-module.exports.bbq_store = (req, res, next) => {
+module.exports.bbq_store = async (req, res, next) => {
 
-  const product_bbq = new Model_bbq({
+  const find = await Model_bbq.exists({id: parseInt(req.body.id)});
+
+  if(find) {
+   return res.status(409).send(`Error: Another Product with the 'ID' ${req.body.id} already exists!`)
+  }
+
+  const product_bbq = await new Model_bbq({
     id: req.body.id,
     name: req.body.name,
     price: req.body.price,
     date: req.body.date
   });
-
-  // validator new way
+  
   product_bbq.save((err) => {
     if (err) {
       return next(res.status(400).send(err.message));
@@ -34,14 +37,12 @@ module.exports.bbq_store = (req, res, next) => {
     }
   });
 
-  /*
-   validator old way
+  /* VALIDATOR OLD WAY
+
    if(!product_bbq.id || !product_bbq.name || !product_bbq.price) {
      return res.status(400).json({msg: 'Please include an ID, a Name and a Price'});  
    }
    */
-  
-  
 };
 
 // INDEX
@@ -57,39 +58,70 @@ module.exports.bbq_index = (req, res) => {
 };
 
 // SHOW by id                                                              
-module.exports.bbq_show= (req, res) => {
+module.exports.bbq_show = async (req, res) => {
+
+  const find = await Model_bbq.exists({id: req.params.id});
+
+  if(find){
+    Model_bbq.find({id: req.params.id}).then((bbq) => {
+      res.json(bbq);
+    });
+  } else {
+    res.status(400).send(`No product with the 'ID' of ${req.params.id}`);
+  }
+
+  /* OLD WAY
 
   Model_bbq.find({id: req.params.id}).then((bbq) => {
+    console.log(find);
     res.json(bbq)
   }).catch((e) => {
     console.log(e);
     res.status(400).send("Something went wrong, please make sure to give a valid 'ID'!")
     // res.status(400).json(`No product with the id of ${req.params.id}`)
   });
-
+  */
 };
 
 // UPDATE (put)
-module.exports.bbq_update = (req, res) => {
+module.exports.bbq_update = async (req, res) => {
   
-  /*
+  const find = await Model_bbq.exists({id: req.params.id});
+
+  if(find){
+    Model_bbq.findOneAndUpdate({id: req.params.id}, {$set: req.body}, {new: true}).then((bbq) => {
+      res.json(bbq)
+    });
+  } else {
+    res.status(500).send("Something went wrong! Did you pass the correct 'ID'?");
+  }
+
+  /* NOTES
   find in the DB the first data that matches the criteria: {id: req.params.id}. Then, change the KEY name into VALUE from 'req.body.name'. 
   Note: {new: true} is for the function to return with the updated values and not the original before updating. 
   findOneAndUpdate(conditions, update, options).
   {$set:{name: req.body.name}} for individual items
   */
 
+  /* OLD WAY
   Model_bbq.findOneAndUpdate({id: req.params.id}, {$set: req.body}, {new: true}).then((bbq) => {
     res.json(bbq)
   }).catch((e) => {
-    // console.log(e);
+    console.log(e);
     res.status(500).send("Something went wrong! Did you pass the correct info?")
   });
+  */
 
 };
 
 // DELETE
-module.exports.bbq_delete = (req, res) => {
+module.exports.bbq_delete = async (req, res) => {
+
+  const find = await Model_bbq.exists({id: parseInt(req.params.id)});
+
+  if(!find) {
+    return res.status(404).send(`Error: The Product with the 'ID' ${req.params.id} does not exist!`)
+   }
   
   Model_bbq.find({id: req.params.id}).deleteOne().then((bbq) => {
     res.status(204).send();
